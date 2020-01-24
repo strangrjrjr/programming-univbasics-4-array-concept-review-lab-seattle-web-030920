9252fcca564644543bb8d532edce6c4586522d88
Roger Robertson Jr <rjrjr@pm.me>
2020-01-24 19:10:51 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,10 +2,10 @@ def find_element_index(array, value_to_find)
   # Add your solution here
   index = 0
   array.length.times do
-    index +=1
     if array[index] == value_to_find
       return index
     end
+    index += 1
   end
   return nil
 end
